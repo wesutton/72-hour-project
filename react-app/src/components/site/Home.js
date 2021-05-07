068c4f8b49d86f24b03f5090a4bf4b8257a3bbae
will <wesutton715@gmail.com>
2021-05-07 15:32:53 -0400

    develop branch react setup

---
+++ react-app/src/components/site/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+function Home() {
+  return (
+    <div className="home" href ="/">
+      <p> Welcome to our 72 hour project</p>
+
+    </div>
+  );
+}
+
+export default Home;
