f59bb0a9e07df1cfabfa326a97984471830c5acc
aymiller200 <aymiller200@gmail.com>
2021-05-09 17:51:50 -0400

    My fetch, and also a mock weather api fetch just so you can see what's going on. It takes a second to get latitude and longitude, so once those load, then buttons populate whick fire off the nasa fetch and the weather fetch. Haven't been able to get it to fetch without the buttons, because of how long it takes to get the current location. Also did some light styling, feel free to change it, I was just messing around with some stuff.

--- react-app/src/components/apps/Nasa api/index.js
+++ react-app/src/components/apps/Nasa api/index.js
@@ -1,48 +1,80 @@
 import "./styles.scss"
 import {useEffect, useState} from 'react'
+import {Container, Col, Row} from 'reactstrap'
 
-// const getDate = () => {
-//     const date = new Date(Data.now()).toDateString()
-//     return date
-// }
+
+
 
 const SatImg = () => {
-
-    // const baseUrl = "https://api.nasa.gov/planetary/earth/imagery"
-
-    //const date = new Date(Data.now()).toDateString()
     
-    const url = `https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&date=2018-01-01&dim=0.15&api_key=`
-    const key = "bphLAKHvd9YWVncp44lf6R8KCxT8eDomyxy7ZSZd"
-    
-   const [image, setImage] = useState()
+    const [image, setImage] = useState()
+    const [weather, setWeather] = useState()
+    const [latitude, setLatitude] = useState()
+    const [longitude, setLongitude] = useState()
 
-    const data = async() => {
-        const res = await fetch(url + key, {
-            headers: 
-            {'Content-Type': 'application/json'}
+    const getLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setLatitude(position.coords.latitude)
+            setLongitude(position.coords.longitude)
         })
+    }
+
+    const currentDate = new Date()
+    let date = currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1) + '-' + currentDate.getDate();
+
+    
+    const data = async () => {
+        const key = "bphLAKHvd9YWVncp44lf6R8KCxT8eDomyxy7ZSZd"
+        const res = await fetch(`https://api.nasa.gov/planetary/earth/assets?lon=${longitude}&lat=${latitude}&date=${date}&dim=0.037&api_key=${key}`)
         const json = await res.json()
         setImage(json)
         console.log(json)
     }
 
-    useEffect(() =>{
-        data()
+    const dataWeather = async () => {
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${latitude}&lon=${longitude}&appid=`
+        const key = "be1c2e173a69af7a3a9beff74909e0fc"
+        const res = await fetch(url + key)
+        const json = await res.json()
+        setWeather(json)
+        console.log('hello', json) 
+    }
+
+    useEffect(() =>{ 
+        getLocation()
     }, [])
-
-    
-
-
 
     return(
         <div>
-            <h3>Here's your location</h3>
-            <img src={image?.url} alt="Sat img"></img>
-            
-
-           
+        < Container className ="main">
+            <Row>
+             <Col md={10} xs={10} className="textDiv"> 
+            <h3>You Are Here: </h3>
+            {longitude ? <p>Longitude: {longitude} </p> : <p>Searching for longitude</p>}
+            {latitude ? <p>Latitude: {latitude} </p> : <p>Searching for latitude</p>}
+            </Col>
+            </Row>
+            <Row>
+            <Col md={10} xs={10} className="imgDiv">
+            {longitude && latitude ? <button onClick={data}>Get Image</button> : null}
+            {image?.url ? <img src={image?.url} alt="Sat img"></img> : null} 
+            </Col>
+            </Row>
+        </Container>
+        <div>
+        <Container className="weatherContainer">
+            <Row>
+                <Col>
+                {longitude && latitude ? <button onClick={dataWeather}>Get Weather</button> : null}
+                 <p>{weather?.weather[0]?.description}</p>
+                 <p>{weather?.main?.temp}</p>
+                </Col>
+            </Row>
+        </Container>
         </div>
+        </div>
+       
     )
 }
 
--- react-app/src/components/site/Navbar.js
+++ react-app/src/components/site/Navbar.js
@@ -1,11 +1,7 @@
 
 import {
-    Route,
-    Link,
     Switch
 } from 'react-router-dom'
-import Home from './Home'
-import Weather from '../apps/Weather-api'
 import SatImg from '../apps/Nasa api/index'
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
 import '../../App.scss'
@@ -13,27 +9,18 @@ import '../../App.scss'
 
 const Header = () => {
     return (
+        <div>
     <header>
         <Navbar className = "header">
-            <NavbarBrand href="/">Home</NavbarBrand>
+            <NavbarBrand>GET GRUB</NavbarBrand> 
               <Nav navbar>
-                <NavItem>
-              <NavLink className="navLink" href ="/weather-api"> Weather API</NavLink>
-              </NavItem>
               <NavItem>
-              <NavLink className="navLink" href ="/nasa-image">Nasa Earth Imagery</NavLink>
               </NavItem>
               </Nav>
-        </Navbar>
-            
-            
-        <Switch>
-                <Route exact path ="/Home"><Home /></Route>
-                <Route exact path ="/"><Home /></Route>
-                <Route exact path = "/weather-api"><Weather /></Route>
-                <Route exact path = "/nasa-image"><SatImg /></Route>
-        </Switch>
+        </Navbar>        
     </header>
+        <SatImg />
+    </div>
      );
 };
 
