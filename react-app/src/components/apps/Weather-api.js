44834df0f1dd08090ef600be5b3ac3110db0fda4
will <wesutton715@gmail.com>
2021-05-10 22:43:55 -0400

    updated fetch. (error: data.map is not a function)

--- react-app/src/components/apps/Weather-api.js
+++ react-app/src/components/apps/Weather-api.js
@@ -1,11 +1,40 @@
-import React from 'react'
-
+import {useEffect, useState} from "react";
 
 const Weather = () => {
-    return (
-        <h3>Weather app</h3>
-    )
+    const url = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=39.7684&lon=86.1480&units=metric&appid=a3dedbc9c9e20947fd59ab3a889ad517';
+    const [data, setData] = useState();
 
-}
+    const initData = async () => {
+        const res = await fetch (url, {
+            headers: {
+                "Content-Type" : "application/json",
+            }
+            })
+            const json = await res.json();
+            setData(json);
+            console.log(json);
+
+    };
+    
+    useEffect(() => {
+        initData();
+    },[]);
+
+    return (
+        <div>
+             {data?.map((temperature) => {
+                 return(
+                     <div>
+                     <h3>{temperature.main.temp}</h3>
+                     </div>
+                 );
+             })}
+        </div>
+    );
+
+};
+
 
 export default Weather;
+
+
